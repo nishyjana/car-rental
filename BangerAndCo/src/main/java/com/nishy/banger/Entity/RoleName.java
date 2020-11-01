7fb1bd4a8641b5b45a2494314b4156d708360124
nishy <nishyjana6435@gmail.com>
2020-11-01 14:26:59 +0530

    final

--- BangerAndCo/src/main/java/com/nishy/banger/Entity/RoleName.java
+++
@@ -1,6 +0,0 @@
-package com.nishy.banger.Entity;
-
-public enum RoleName {
-    ROLE_USER,
-    ROLE_ADMIN
-}
--- BangerAndCo/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-insert into car values (14,TRUE,'toyata','bhv66','car','petrol','https://imgd.aeplcdn.com/600x337/cw/ec/39545/Toyota-Glanza-Right-Front-Three-Quarter-159520.jpg?wm=0&q=85','rs2')
-insert into car values (13,TRUE,'toyata','bhv66','car','petrol','https://media.zigcdn.com/media/model/2016/Oct/camry-hybrid_right_600x300.jpg','rs2')
-
-insert into car values (12,TRUE,'toyata','bhv66','car','petrol','https://media.zigcdn.com/media/model/2019/Jan/toyota-camry-2019-right_600x300.jpg','rs2')
-
-insert into car values (11,TRUE,'toyata','bhv66','car','petrol','https://auto.ndtvimg.com/car-images/big/toyota/camry/toyota-camry.jpg?v=29','rs2')
-INSERT INTO role(name ) VALUES('ROLE_USER')
-INSERT INTO role(name ) VALUES('ROLE_ADMIN')
-insert into customers values (1,'nishyman','adfgdggmin','12fgffg3','123','123123',TRUE,'admin')
